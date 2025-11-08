3ae51dcd0f9a998e7c1d532f8faf1eb41a498061
Luca Vivona <lucavivona01@gmail.com>
2025-11-07 22:44:12 -0500

    feat: impl unet logic

--- src/lib.rs
+++ src/lib.rs
@@ -8,41 +8,106 @@ use burn::tensor::{Tensor, backend::Backend};
 mod block;
 pub use block::{DecoderBlock, DecoderBlockConfig};
 
-use crate::block::{Conv2dBlock, EncoderBlock, EncoderBlockConfig};
+use crate::block::{Conv2dBlock, Conv2dBlockConfig, EncoderBlock, EncoderBlockConfig};
 
-///
+const DEFAULT_BLOCK_1_FEATRUES: usize = 64;
+const DEFAULT_BLOCK_2_FEATRUES: usize = 128;
+const DEFAULT_BLOCK_3_FEATRUES: usize = 258;
+const DEFAULT_BLOCK_4_FEATRUES: usize = 512;
+const DEFAULT_BLOCK_5_FEATRUES: usize = 1024;
+
+/// Configuration to create a Unet block, uisng [init function](UnetConfig::init)
 #[derive(Config, Debug)]
 pub struct UnetConfig {
-    num_channels: usize,
-    num_classes: usize,
-    dropout: Option<f64>,
+    /// The number of input channels
+    pub num_channels: usize,
+    /// The number of output classes.
+    pub num_classes: usize,
 }
 
 impl UnetConfig {
+    /// Initialize a new [unet](Unet) module.
     ///
+    /// Inspired [Unet](https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py) block.
     pub fn init<B: Backend>(self, device: &B::Device) -> Unet<B> {
-        todo!()
+        let UnetConfig {
+            num_channels,
+            num_classes,
+            ..
+        } = self;
+
+        Unet {
+            inc: Conv2dBlockConfig::new(num_channels, 64).init(device),
+            encoder_block_1: EncoderBlockConfig::from((
+                DEFAULT_BLOCK_1_FEATRUES,
+                DEFAULT_BLOCK_2_FEATRUES,
+            ))
+            .init(device),
+            encoder_block_2: EncoderBlockConfig::from((
+                DEFAULT_BLOCK_2_FEATRUES,
+                DEFAULT_BLOCK_3_FEATRUES,
+            ))
+            .init(device),
+            encoder_block_3: EncoderBlockConfig::from((
+                DEFAULT_BLOCK_3_FEATRUES,
+                DEFAULT_BLOCK_4_FEATRUES,
+            ))
+            .init(device),
+            encoder_block_4: EncoderBlockConfig::from((
+                DEFAULT_BLOCK_4_FEATRUES,
+                DEFAULT_BLOCK_5_FEATRUES,
+            ))
+            .init(device),
+            decoder_block_1: DecoderBlockConfig::from((
+                DEFAULT_BLOCK_5_FEATRUES,
+                DEFAULT_BLOCK_4_FEATRUES,
+            ))
+            .init(device),
+            decoder_block_2: DecoderBlockConfig::from((512, DEFAULT_BLOCK_3_FEATRUES)).init(device),
+            decoder_block_3: DecoderBlockConfig::from((
+                DEFAULT_BLOCK_3_FEATRUES,
+                DEFAULT_BLOCK_2_FEATRUES,
+            ))
+            .init(device),
+            decoder_block_4: DecoderBlockConfig::from((
+                DEFAULT_BLOCK_2_FEATRUES,
+                DEFAULT_BLOCK_1_FEATRUES,
+            ))
+            .init(device),
+            output: Conv2dConfig::new([DEFAULT_BLOCK_1_FEATRUES, num_classes], [1, 1]).init(device),
+        }
     }
 }
 
+/// Applies a Unet operation over the input tensors.
 ///
+/// Should be created with [UnetConfig].
 #[derive(Module, Debug)]
 pub struct Unet<B: Backend> {
-    inc: Conv2dBlock<B>,
-    encoder_block_1: EncoderBlock<B>,
-    encoder_block_2: EncoderBlock<B>,
-    encoder_block_3: EncoderBlock<B>,
-    encoder_block_4: EncoderBlock<B>,
-    decoder_block_1: DecoderBlock<B>,
-    decoder_block_2: DecoderBlock<B>,
-    decoder_block_3: DecoderBlock<B>,
-    decoder_block_4: DecoderBlock<B>,
-    output: Conv2d<B>,
+    /// Input projection block.
+    pub inc: Conv2dBlock<B>,
+    /// Intermideate encoder block.
+    pub encoder_block_1: EncoderBlock<B>,
+    /// Intermideate encoder block.
+    pub encoder_block_2: EncoderBlock<B>,
+    /// Intermideate encoder block.
+    pub encoder_block_3: EncoderBlock<B>,
+    /// Intermideate bottleneck block.
+    pub encoder_block_4: EncoderBlock<B>,
+    /// Intermideate decoder upsample block.
+    pub decoder_block_1: DecoderBlock<B>,
+    /// Intermideate decoder upsample block.
+    pub decoder_block_2: DecoderBlock<B>,
+    /// Intermideate decoder upsample block.
+    pub decoder_block_3: DecoderBlock<B>,
+    /// Final decoder block.
+    pub decoder_block_4: DecoderBlock<B>,
+    /// Ouput logits projection, shape `[channel, height, width]`.
+    pub output: Conv2d<B>,
 }
 
 impl<B: Backend> Unet<B> {
     /// Applies the foward pass through the U-net.
-    ///
     pub fn foward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
         let x1 = self.inc.forward(input);
         let x2 = self.encoder_block_1.forward_ref(&x1);
